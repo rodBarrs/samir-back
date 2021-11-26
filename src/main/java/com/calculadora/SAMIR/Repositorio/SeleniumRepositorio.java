b4263a44b6fbec96b000bfd99216095490af232a
Moises Pompilio <moisesjtmed@gmail.com>
2021-11-25 23:04:30 -0300

    addd

--- src/main/java/com/calculadora/SAMIR/Repositorio/SeleniumRepositorio.java
+++ src/main/java/com/calculadora/SAMIR/Repositorio/SeleniumRepositorio.java
@@ -15,17 +15,7 @@ public class SeleniumRepositorio {
 	public void open(String url) throws InterruptedException {
 	 driver = new ChromeDriver();
 		driver.get(url);
-		String emailPath = "#email";
 		
-		WebElement emailElemt =   driver.findElement(By.cssSelector(emailPath));
-		
-		emailElemt.sendKeys("moisesjt@r7.com");
-		String senhaPath = "pass";
-		WebElement senhaElemt =   driver.findElement(By.id(senhaPath));
-		senhaElemt.sendKeys("93034554jt");
-		String butonPath = "/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button";
-		WebElement butonElem =   driver.findElement(By.xpath(butonPath));
-		butonElem.click();
 		Thread.sleep(3000); 
 	}
 	
