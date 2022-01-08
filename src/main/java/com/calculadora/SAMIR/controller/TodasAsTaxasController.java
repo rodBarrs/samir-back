96e9445adde228246b5599df3611b55fe811e3e9
Moises Pompilio <moisesjtmed@gmail.com>
2022-01-08 13:07:33 -0300

    taxas

--- src/main/java/com/calculadora/SAMIR/controller/TodasAsTaxasController.java
+++ src/main/java/com/calculadora/SAMIR/controller/TodasAsTaxasController.java
@@ -28,61 +28,9 @@ public class TodasAsTaxasController {
 	@Autowired
 	private ReajusteRepositorio reajusteRepository;
 
-	@SuppressWarnings("deprecation")
-	@GetMapping("todas/{juros}/{correcao}")
-	public TodasAsTaxasModelo[] todasTaxas(@PathVariable("juros") int ju, @PathVariable("correcao") int cor) {
-		TodasAsTaxasModelo[] lista = new TodasAsTaxasModelo[10000];
 
-		List<Juros> juros = jurosRepository.findByTipoOrderByCodigoAsc(ju);
-		List<TaxaDeCorrecao> correcao = correcaoRepository.findByTipoOrderByCodigoAsc(cor);
-		List<TaxaReajuste> reajuste = reajusteRepository.findAll();
-		int indiceReajuste = 0;
-		int indiceJuros = 0;
-		int indiceCorrecao = 0;
 
-		for (int i = 0; i <= 1000; i++) {
-
-			if (indiceReajuste <= reajuste.size() - 1) {
-				while (reajuste.get(indiceReajuste).getData() != correcao.get(indiceCorrecao).getData() && reajuste
-						.get(indiceReajuste).getData().getYear() != correcao.get(indiceCorrecao).getData().getYear()) {
-
-					indiceCorrecao++;
-					if (indiceCorrecao == correcao.size() - 1) {
-						indiceCorrecao = 0;
-						break;
-					}
-
-				}
-				while (reajuste.get(indiceReajuste).getData() != juros.get(indiceJuros).getData() && reajuste
-						.get(indiceReajuste).getData().getYear() != juros.get(indiceJuros).getData().getYear()) {
-					indiceJuros++;
-					if (indiceJuros == juros.size() - 1) {
-						indiceJuros = 0;
-					}
-
-				}
-				TodasAsTaxasModelo taxa = new TodasAsTaxasModelo();
-				if (indiceJuros <= juros.size() - 1 && taxa.getData() == juros.get(indiceJuros).getData()) {
-					taxa.setJurosAcumulado(juros.get(indiceJuros).getJurosAcumulados());
-				}
-				if (indiceCorrecao <= correcao.size() - 1 && taxa.getData() == correcao.get(indiceCorrecao).getData()) {
-					taxa.setCorrecaoAcumulado(correcao.get(indiceCorrecao).getTaxaAcumulada());
-				}
-				taxa.setData(reajuste.get(indiceReajuste).getData());
-				taxa.setReajuste(reajuste.get(indiceReajuste).getReajusteAcumulado());
-				lista[i] = taxa;
-			}
-
-			indiceCorrecao++;
-			indiceJuros = 0;
-			indiceReajuste++;
-		}
-
-		return lista;
-
-	}
-
-	@SuppressWarnings("deprecation")
+	
 	@GetMapping("todasTaxas/{juros}/{correcao}")
 	public TodasAsTaxasModelo[] Taxas(@PathVariable("juros") int ju, @PathVariable("correcao") int cor) {
 		TodasAsTaxasModelo[] lista = new TodasAsTaxasModelo[10000];
@@ -91,7 +39,6 @@ public class TodasAsTaxasController {
 		List<TaxaDeCorrecao> correcao = correcaoRepository.findByTipoOrderByCodigoAsc(cor);
 		List<TaxaReajuste> reajuste = reajusteRepository.findAll();
 		int indiceReajuste = 0;
-		int indiceJuros = 0;
 		int indiceCorrecao = 0;
 		int j = 2;
 		for (int i = 0; i < juros.size(); i++) {
