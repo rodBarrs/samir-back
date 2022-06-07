571400dfb8fa33b9a220043f18febb6092ee5601
rodBarrs <rodrigosbarros51@gmail.com>
2022-06-07 14:20:12 -0300

    adioção dos atributos Calculo em lote

--- src/main/java/com/calculadora/SAMIR/Modelo/CalculoEmLote.java
+++ src/main/java/com/calculadora/SAMIR/Modelo/CalculoEmLote.java
@@ -1,7 +1,23 @@
 package com.calculadora.SAMIR.Modelo;
 
+import java.util.ArrayList;
+
 public class CalculoEmLote {
 
-
+    private int numeroDoProcesso;
+    private String nome;
+    private String dataDeAjuizamento;
+    private String cpf;
+    private String termoInicial;
+    private String termoFinal;
+    private String rmi;
+    private String beneficio;
+    private String numeroDoBeneficio;
+    private String dataDePagamento;
+    private String citacao;
+    private String aps;
+    private BeneficioAcumulado[] beneficioAcumulados;
+    private String honorarioAdvocativosData;
+    private float  honorariosAdvocativos
 
 }
